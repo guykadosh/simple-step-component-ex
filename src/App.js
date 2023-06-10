52c28258f228a3611a7b50bcc835f4080bb7c257
Guy Kadosh <guyk50@gmail.com>
2023-06-10 16:56:12 +0300

    added rusable stepmessage

--- src/App.js
+++ src/App.js
@@ -41,10 +41,7 @@ function Steps() {
             <div className={step >= 2 ? 'active' : ''}>2</div>
             <div className={step >= 3 ? 'active' : ''}>3</div>
           </div>
-          <p className="message">
-            Step {step}: {messages[step - 1]}
-          </p>
-
+          <StepMessage step={step}>{messages[step - 1]}</StepMessage>
           <div className="buttons">
             <Button
               handleClick={handlePrevious}
@@ -63,6 +60,14 @@ function Steps() {
   )
 }
 
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step}</h3>
+      {children}
+    </div>
+  )
+}
 function Button({ handleClick, bgColor, textColor, children }) {
   return (
     <button
